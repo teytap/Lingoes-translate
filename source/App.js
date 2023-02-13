ca9b02861357d44951bdbee5daa9f796d8dab4ef
Mehtap Tataroğlu <teytap@gmail.com>
2023-02-13 18:52:48 +0300

    added api and functions

--- source/App.js
+++ source/App.js
@@ -1,20 +1,42 @@
-function searchFormWord(event) {
-  event.preventDefault();
-  let newWordInput = document.querySelector("#newWord").value;
-  // searchWord(newWordInput);
-  let h2 = document.querySelector("h2");
-  let meaningOf = document.querySelector("#meaningOf");
-  h2.innerHTML = newWordInput;
-  meaningOf.innerHTML = `Meaning of ${newWordInput} is ... `;
+const submitBtn = document.querySelector("#submit-btn");
+const selectLanguage1 = document.querySelector("#languages-one");
+const selectLanguage2 = document.querySelector("#languages-two");
+const languageResult = document.querySelector(".language-result");
+const translationResult = document.querySelector(".translation");
+
+function translate(response) {
+  // languageResult.innerHTML =
+  //   selectLanguage2.options[selectLanguage2.selectedIndex].text;
+  translationResult.innerHTML = response.responseData.translatedText;
+}
+function showTranslate(textInput, twoLanguages) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e5cb67fa32msha237b8afe927e3fp13f481jsn269ad7101412",
+      "X-RapidAPI-Host":
+        "translated-mymemory---translation-memory.p.rapidapi.com",
+    },
+  };
+  fetch(
+    `https://translated-mymemory---translation-memory.p.rapidapi.com/get?langpair=${twoLanguages}&q=${textInput}&mt=1&onlyprivate=0&de=a%40b.c`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => translate(response))
+    .catch((err) => console.error(err));
 }
 
-function listenWord(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  alert("Listen to pronounciation");
+  const textInput = document.querySelector("#newWord").value;
+  const firstLanguage = selectLanguage1.value;
+  const secondLanguage = selectLanguage2.value;
+  let twoLanguages = `${firstLanguage}|${secondLanguage}`;
+
+  showTranslate(textInput, twoLanguages);
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", searchFormWord);
-let listen = document.querySelector(".listen");
-listen.addEventListener("click", listenWord);
-let listen2 = document.querySelector(".listen2");
-listen2.addEventListener("click", listenWord);
+
+submitBtn.addEventListener("click", handleSubmit);
+
+showTranslate("Hello", "en|tr");
